e6741780f43dcf3be9e1b95e01ba342173a8ec52
spacedrone404 <81930707+spacedrone404@users.noreply.github.com>
2025-10-29 21:30:04 +0300

    enlarge buttons 6

--- src/components/DimensionsDetector.jsx
+++ src/components/DimensionsDetector.jsx
@@ -1,8 +1,8 @@
 // Used for detection of screen dimensions + beta sign in the corner
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const ViewportWidthIndicator = () => {
+const BetaNewIndicator = () => {
   return (
     <div
       style={{
@@ -48,75 +48,62 @@ const ViewportWidthIndicator = () => {
   );
 };
 
+const ViewportWidthIndicator = () => {
+  const [width, setWidth] = useState(window.innerWidth); // Init width
+  const [height, setHeight] = useState(window.innerHeight); // Init height
+
+  // Getting window size
+  useEffect(() => {
+    const handleResize = () => {
+      const newWidth = window.innerWidth;
+      const newHeight = window.innerHeight;
+
+      if (newWidth !== width) {
+        setWidth(newWidth);
+      }
+
+      if (newHeight !== height) {
+        setHeight(newHeight);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty array of dependacies equals single exectution upon deploying component
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        bottom: "0",
+        left: "214px",
+        transform: "translate(-50%, 0)",
+        padding: "10px",
+        opacity: "64%",
+        fontSize: "15px",
+        color: "#FF0000",
+        backgroundColor: "#FFF",
+        outline: "2px solid red",
+        textAlign: "center",
+        boxShadow: "0 0 5px rgba(0, 0, 0, 0.2)",
+        zIndex: 2100000,
+      }}
+    >
+      width: {width}px / height: {height}px
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <div>
+      <BetaNewIndicator />
       <ViewportWidthIndicator />
     </div>
   );
 };
 
 export default App;
-
-// Dimension detector
-
-// import React, { useState, useEffect } from "react";
-
-// const ViewportWidthIndicator = () => {
-//   const [width, setWidth] = useState(window.innerWidth); // Init width
-//   const [height, setHeight] = useState(window.innerHeight); // Init height
-
-//   // Getting window size
-//   useEffect(() => {
-//     const handleResize = () => {
-//       const newWidth = window.innerWidth;
-//       const newHeight = window.innerHeight;
-
-//       if (newWidth !== width) {
-//         setWidth(newWidth);
-//       }
-
-//       if (newHeight !== height) {
-//         setHeight(newHeight);
-//       }
-//     };
-
-//     window.addEventListener("resize", handleResize);
-
-//     return () => {
-//       window.removeEventListener("resize", handleResize);
-//     };
-//   }, []); // Empty array of dependacies equals single exectution upon deploying component
-
-//   return (
-//     <div
-//       style={{
-//         position: "fixed",
-//         bottom: "0",
-//         left: "214px",
-//         transform: "translate(-50%, 0)",
-//         padding: "10px",
-//         opacity: "64%",
-//         fontSize: "15px",
-//         color: "#FF0000",
-//         backgroundColor: "#FFF",
-//         outline: "2px solid red",
-//         textAlign: "center",
-//         boxShadow: "0 0 5px rgba(0, 0, 0, 0.2)",
-//         zIndex: 2100000,
-//       }}
-//     >
-//       Debugging viewport width: {width} px / height: {height} px
-//     </div>
-//   );
-// };
-
-// const App = () => {
-//   return (
-//     <div>
-//       <ViewportWidthIndicator />
-//     </div>
-//   );
-// };
-
-// export default App;
